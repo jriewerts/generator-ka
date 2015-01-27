35ae829a9b6e3761ebf7bba13c02f2cc0001fbba
John Riewerts <jriewerts@gmail.com>
2015-01-27 11:41:46 -0600

    cleaned up bower and npm conflicts

--- app/index.js
+++ app/index.js
@@ -33,7 +33,7 @@ proto.composeKrakenApp = function () {
         UIPackageManager: 'bower',
         cssModule: 'less',
         jsModule: false,  //todo: we need to actually file bug and fix the 'when' resolver in prompts.js to false is a valid value
-        'skip-install-npm': true,
+        'skip-install-npm': false,
         'skip-install-bower': true,
         'skip-install': true
       },
@@ -74,6 +74,41 @@ proto.postAngular = function () {
 
 };
 
-proto.mergeMagic = function() {
-  //todo
+proto.showWarning = function() {
+  this.log('****************************************');
+  this.log('*                                      *');
+  this.log('* The next several operations will     *');
+  this.log('* cause conflicts to happen. This is   *');
+  this.log('* because we are merging the two       *');
+  this.log('* generators together. Hit \'y\' in the  *');
+  this.log('* coming prompts.                      *');
+  this.log('*                                      *');
+  this.log('****************************************');
+};
+
+proto.mergingItTogether = function() {
+
+  //bower cleanup
+  this.fs.move('public/bower.json', 'bower.json');
+  this.fs.delete('.bowerrc'); //we will replace this later
+
+  //merge package.json...
+  var pkgKraken = this.fs.readJSON('package.json');
+  var pkgAngular = this.fs.readJSON('public/package.json');
+
+  //merge dev dependencies, use kraken dep versions as base
+  for(var dep in pkgAngular.devDependencies) {
+    if(!pkgKraken.devDependencies[dep]) {
+      pkgKraken.devDependencies[dep] = pkgAngular.devDependencies[dep];
+    }
+  }
+
+  //overwrite base package json and delete cg-angular's
+  this.fs.writeJSON('package.json', pkgKraken);
+  this.fs.delete('public/package.json');
+
+  //push static additions to the project
+  this.directory('main/', './')
+
+  
 };
